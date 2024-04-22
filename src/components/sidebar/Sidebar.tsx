35df12c270d3e86bb708fc359d132cc7e5e60e1a
mustaqimbd <mustaqimkhanbd@gmail.com>
2024-04-22 19:10:39 +0600

    [skiped] processing orders route

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -84,7 +84,7 @@ export function Sidebar() {
             <span>Orders</span>
           </span>
         </Link>
-        <Link href="/dashboard/processing-orders">
+        {/* <Link href="/dashboard/processing-orders">
           <span
             className={cn(
               "group flex gap-2 items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
@@ -96,7 +96,7 @@ export function Sidebar() {
             <PlusCircle className="w-4 h-4" />
             <span>Processing Orders</span>
           </span>
-        </Link>
+        </Link> */}
 
         <Link href="/dashboard/all-claim-request">
           <span
