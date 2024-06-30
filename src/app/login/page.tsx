d73121ddb26bfe2dff7f089e9045830a59460891
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-06-30 15:56:08 +0600

    [done] customer order notes on view details page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,14 +7,8 @@ import { setUser } from "@/redux/features/auth/authSlice";
 import { TUser } from "@/redux/features/auth/interface";
 import { useAppDispatch } from "@/redux/hooks";
 import decodeJWT from "@/utilities/decodeJWT";
-// import { Metadata } from "next";
 import { useRouter } from "next/navigation";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-
-// export const metadata: Metadata = {
-//   title: "Login",
-//   description:"Oneself"
-// };
 
 const LoginPage = () => {
   const dispatch = useAppDispatch();
@@ -48,56 +42,58 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="w-[430px] border-none md:mx-auto my-6">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      >
-        <div>
-          <h4 className="text-center my-3 font-bold  text-2xl">Login Page</h4>
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="phoneEmailOrUid"
-          >
-            Phone Number
-          </label>
-          <Input
-            className={`shadow appearance-none border ${errors.phoneEmailOrUid ? "border-red-500" : "border-gray-200"} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
-            type="tel"
-            placeholder="Enter Your Phone Number"
-            id="phoneEmailOrUid"
-            {...register("phoneEmailOrUid")}
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <Input
-            className={`shadow appearance-none border ${errors.password ? "border-red-500" : "border-gray-200"} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            type="password"
-            id="password"
-            placeholder="Enter a Password"
-            {...register("password")}
-            required
-          />
-        </div>
-        <div className="flex flex-col items-center justify-between gap-5">
-          <EcButton
-            type="submit"
-            disabled={isLoading}
-            className="w-full  font-bold py-2 px-4 bg-primary"
-          >
-            Login
-          </EcButton>
-        </div>
-      </form>
+    <div className="flex justify-center h-screen items-center">
+      <div className="w-[430px] border-none md:mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div>
+            <h4 className="text-center my-3 font-bold  text-2xl">Login Page</h4>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="phoneEmailOrUid"
+            >
+              Phone Number
+            </label>
+            <Input
+              className={`shadow appearance-none border ${errors.phoneEmailOrUid ? "border-red-500" : "border-gray-200"} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              type="tel"
+              placeholder="Enter Your Phone Number"
+              id="phoneEmailOrUid"
+              {...register("phoneEmailOrUid")}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <Input
+              className={`shadow appearance-none border ${errors.password ? "border-red-500" : "border-gray-200"} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+              type="password"
+              id="password"
+              placeholder="Enter a Password"
+              {...register("password")}
+              required
+            />
+          </div>
+          <div className="flex flex-col items-center justify-between gap-5">
+            <EcButton
+              type="submit"
+              disabled={isLoading}
+              className="w-full  font-bold py-2 px-4 bg-primary"
+            >
+              Login
+            </EcButton>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
