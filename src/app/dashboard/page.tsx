fd330cf91c075d9f7056180be5f29038c66359c6
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-09-24 15:32:30 +0600

    [done] add image and phone number on homepage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,8 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import config from "@/config/config";
 import { getProfile } from "@/lib/getAccessToken";
 import { Metadata } from "next";
+import dummyUser from "../../../public/icons/user.jpg";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -8,11 +11,21 @@ export const metadata: Metadata = {
 
 const Dashboard = async () => {
   const user = await getProfile();
+  const profilePicUrl = user?.profilePicture
+    ? `${config.base_url}/${user?.profilePicture}`
+    : dummyUser.src;
   return (
     <div className="flex justify-center items-center h-full">
-      <h2 className="text-center font-bold text-2xl">
-        Welcome, {user.fullName}
-      </h2>
+      <div>
+        <div className="flex justify-center">
+          <Avatar className="rounded-full w-28 h-28">
+            <AvatarImage src={profilePicUrl} />
+            <AvatarFallback>{user?.fullName}</AvatarFallback>
+          </Avatar>
+        </div>
+        <h2 className="text-center font-semibold text-xl">{user.fullName}</h2>
+        <h2 className="text-center">{user.phoneNumber}</h2>
+      </div>
     </div>
   );
 };
